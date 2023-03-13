c31dab3cb183b4f97940c9414ff91a9db5f70f4d
Xiaofeng (Steve) Huang <37822738+stevehxf@users.noreply.github.com>
2023-03-13 13:46:52 -0400

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,76 +1,51 @@
 #!/bin/bash
 
-# usage function
-function usage {
-    echo "Usage: $0 [-p] accession_list output_directory"
-    echo "  -p: flag to indicate paired-end sequencing (optional)"
-    echo "  accession_list: file containing a list of accession numbers, one per line"
-    echo "  output_directory: directory where you want to save the fastq files"
-    exit 1
-}
+# This script downloads fastq data from the GEO database for a list of accession numbers
+# Usage: ./download_data.sh [-p] accession_list output_directory
+# Optional flag -p indicates paired-end sequencing. If not provided, single-end sequencing is assumed.
+# Accession list is a file containing a list of accession numbers, one per line.
+# Output directory is the directory where the fastq files will be saved. If the directory does not exist, it will be created.
 
-# check if the correct number of arguments was provided
-if [ $# -lt 2 ]; then
-    usage
+# check for the correct number of arguments
+if [[ $# -lt 2 ]]; then
+    echo "Usage: ./download_data.sh [-p] accession_list output_directory"
+    exit 1
 fi
 
-# default to single-end sequencing
+# set default values
 paired=false
 
 # parse command line arguments
-while getopts "hp" opt; do
-    case ${opt} in
-        h)
-            usage
-            ;;
-        p)
-            paired=true
-            ;;
-        \?)
-            usage
-            ;;
-    esac
+while getopts ":p" opt; do
+  case ${opt} in
+    p )
+      paired=true
+      ;;
+    \? )
+      echo "Usage: ./download_data.sh [-p] accession_list output_directory"
+      exit 1
+      ;;
+  esac
 done
+shift $((OPTIND -1))
 
-# remove the options from the positional parameters
-shift $((OPTIND-1))
+accession_list=$1
+output_directory=$2
 
-# set the accessions file to the first positional parameter
-accessions_file=$1
-
-# check if the accessions file was provided
-if [ -z "$accessions_file" ]; then
-    usage
-fi
-
-# check if the accessions file exists
-if [ ! -f "$accessions_file" ]; then
-    echo "Error: File $accessions_file does not exist"
+# check that the accession list file exists
+if [[ ! -f $accession_list ]]; then
+    echo "Accession list file not found: $accession_list"
     exit 1
 fi
 
-# set the output directory to the second positional parameter
-output_dir=$2
+# create the output directory if it doesn't exist
+mkdir -p $output_directory
 
-# check if the output directory was provided
-if [ -z "$output_dir" ]; then
-    usage
-fi
-
-# check if the output directory exists, if not create it
-if [ ! -d "$output_dir" ]; then
-    mkdir -p "$output_dir"
-fi
-
-# change to output directory
-cd "$output_dir"
-
-# loop through each accession number in the file and download the fastq data
-while read accession_number; do
-    echo "Downloading ${accession_number}..."
-    if [ "$paired" = true ]; then
-        fastq-dump --split-files ${accession_number}
+# download the data for each accession number
+while read accession; do
+    if [[ $paired = true ]]; then
+        fastq-dump --split-files --gzip $accession -O $output_directory
     else
-        fastq-dump ${accession_number}
+        fastq-dump --gzip $accession -O $output_directory
     fi
-done < "$accessions_file"
+done < $accession_list
